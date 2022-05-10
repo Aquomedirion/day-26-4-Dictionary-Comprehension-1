77320687d80f9963fc820229d7e5641d861d9354
aquomedirion <aquomedirion@gmail.com>
2022-05-10 16:55:06 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+list_of_sentence = sentence.split()
+result = {word:len(word) for word  in list_of_sentence}
+
+# Write your code below:
+
+
+
+print(result)
+
